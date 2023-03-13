21a8956f1180a62495a1b8f3aa610c597762b8b2
Hoho-san <javierjojo24@gmail.com>
2023-03-13 14:27:32 +0800

    updating my portfolio

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 document
     .querySelectorAll(".certificate .myCertificate .certificate_title img")
-    .forEach((image) => {
+    .forEach(image => {
         image.onclick = () => {
             document.querySelector(".popup-image").style.display = "block";
             document.querySelector(".popup-image img").src =
@@ -11,3 +11,15 @@ document
 document.querySelector(".popup-image span").onclick = () => {
     document.querySelector(".popup-image").style.display = "none";
 };
+
+var typed = new Typed(".typed", {
+    strings: [
+        "Web DEVELOPER",
+        "FRONT-END DEVELOPER",
+        "BACK-END DEVELOPER",
+        "Pucha, Idol pala ko eh",
+    ],
+    typeSpeed: 70,
+    backSpeed: 30,
+    loop: true,
+});
